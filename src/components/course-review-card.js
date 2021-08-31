81cad362c80e7572dab22994b5cea85f784f288a
Cameron Huang <cameronhuang96@gmail.com>
2021-08-31 16:46:30 +1000

    added star ratings to cards

--- src/components/course-review-card.js
+++ src/components/course-review-card.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Card } from 'semantic-ui-react';
-import RatingExampleDisabled from '../components/star-ratings.js';
+import { Rating } from 'semantic-ui-react';
 
 const CourseReviewCard = (props) => {
   const { code } = props;
@@ -13,7 +13,7 @@ const CourseReviewCard = (props) => {
         <Card.Content>
           <Card.Header>{code}</Card.Header>
           <Card.Meta>Introduction to Programming</Card.Meta>
-          <RatingExampleDisabled/>
+          <Rating defaultRating={3} maxRating={5} disabled />
           <Card.Description>
           This is a long description of the above course describing what the course entails
           and what to expect when you enroll in the course
