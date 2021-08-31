939c7c8c323d06cbc87b09101f291036d7bc040d
Priscilla <tingtangsoo@gmail.com>
2021-08-31 17:34:14 +1000

    Created disclaimer on footer and included CSESoc logo

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,40 +6,36 @@ import CSESocLogo from '../assets/csesoc-logo.svg';
 // This footer will appear on all pages
 const Footer = () => {
   return (
-    <div style={{ backgroundColor: '#3A76F8', padding: '1.5em' }}>
-      <Container style={{ color: 'white' }}>
-        <Grid columns='equal'>
-          <Grid.Column width={3}>
-            <Image src={CSESocLogo} size='small'/>
-            <br/>
-            <p>
+    <Container>
+      <Grid columns='equal'>
+        <Grid.Column width={3}>
+          <Image className='csesoc-logo' src={CSESocLogo} size='small'/>
+          <p>
         © 2021 — CSESoc UNSW
-            </p>
-          </Grid.Column>
-          <Grid.Column>
-            <p>
+          </p>
+        </Grid.Column>
+        <Grid.Column>
+          <p>
         CSESoc is the constituent student society of UNSW’s School of Computer Science and Engineering.
         We do not represent the School, Faculty, or University.
-            </p>
-            <p>
+          </p>
+          <p>
         This website seeks to be a general guide, but its information has not been officially
         endorsed and is subject to change or correction.
         This is not official advice, and you should confirm any statements are correct with regard to
         your situation before relying on it.
         Any opinions expressed are those of the authors, and may not necessarily represent those of
         the University, Faculty, School, or Society.
-            </p>
-            <p>
+          </p>
+          <p>
         You are responsible for any content provided and should only post content which you are
         comfortable with sharing. We reserve the right to remove content from the website if it
         is deemed to be abusive, offensive or otherwise inappropriate. UNSW policies apply and if a
         breach is detected, further action may be taken with the University.
-            </p>
-          </Grid.Column>
-        </Grid>
-      </Container>
-    </div>
-    // Try not to use html elements here - surely there is a Semantic ui component suitable here!
+          </p>
+        </Grid.Column>
+      </Grid>
+    </Container>
   );
 };
 
