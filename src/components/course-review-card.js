590abe44d60ef5ba6299dc950cf2f1e02f6dbdec
Cameron Huang <cameronhuang96@gmail.com>
2021-09-01 12:00:28 +1000

    changed star style to yellow star

--- src/components/course-review-card.js
+++ src/components/course-review-card.js
@@ -13,7 +13,7 @@ const CourseReviewCard = (props) => {
         <Card.Content>
           <Card.Header>{code}</Card.Header>
           <Card.Meta>Introduction to Programming</Card.Meta>
-          <Rating defaultRating={3} maxRating={5} disabled />
+          <Rating icon='star' defaultRating={3} maxRating={5} disabled />
           <Card.Description>
           This is a long description of the above course describing what the course entails
           and what to expect when you enroll in the course
