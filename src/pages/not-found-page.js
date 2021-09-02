a32d3dd6c4010a58418d72964a492397dd6cd8b5
Joey <joeyxie12@gmail.com>
2021-09-02 13:53:04 +1000

    404 page background has been fixed

--- src/pages/not-found-page.js
+++ src/pages/not-found-page.js
@@ -4,11 +4,11 @@ import Error404Image from '../assets/not-found.jpg';
 
 const NotFoundPage = () => {
   return (
-    <div>
-      <Header as='h1'>404 Not Found Page</Header>
-      <Image src={Error404Image} size='massive' centered />
+    <div className='not-found-page-background'>
+      {/* <Header as='h1'>404 Not Found Page</Header> */}
       <h1 align="center">
-        Oops looks like you might be lost in the CyberSpacE
+      Oops looks like you might be lost in the CyberSpacE
+        <br></br>
       </h1>
     </div>
   );
