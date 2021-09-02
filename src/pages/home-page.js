ebb75a8bd7339e58bf5f433aa4cc926a42a0e710
Cameron Huang <cameronhuang96@gmail.com>
2021-09-02 12:46:29 +1000

    BRUH HOW HARD IS IT TO GET TEXT OVERFLOW WORKING AHHHHH

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -49,34 +49,81 @@ const HomePage = () => {
 
       {/* Check out the Dropdown component page for examples of inline dropdowns, and filter dropdowns */}
 
-      <Grid columns={3}>
+      <Grid columns='three'>
         <Grid.Row>
           <Grid.Column>
             <CourseReviewCard code="COMP1511" name="Programming Fundamentals"
-              desc="This is a placeholder for the actual course description."/>
+              desc="An introduction to problem-solving via programming, which
+              aims to have students develop proficiency in using a high level
+              programming language. Topics: algorithms, program structures
+              (statements, sequence, selection, iteration, functions),
+              data types (numeric, character), data structures (arrays, tuples,
+              pointers, lists), storage structures (memory, addresses),
+              introduction to analysis of algorithms, testing, code quality,
+              teamwork, and reflective practice. The course includes extensive
+              practical work in labs and programming projects."/>
           </Grid.Column>
           <Grid.Column>
             <CourseReviewCard code="COMP1521" name="Computer Systems Fundamentals"
-              desc="This is a placeholder for the actual course description."/>
+              desc="This course provides a programmer's view on how a computer
+              system executes programs, manipulates data and communicates. It
+              enables students to become effective programmers in dealing with
+              issues of performance, portability, and robustness. It is
+              typically taken in the term after completing COMP1511, but could
+              be delayed and taken later. It serves as a foundation for later
+              courses on networks, operating systems, computer architecture and
+              compilers, where a deeper understanding of systems-level issues is
+              required."/>
           </Grid.Column>
           <Grid.Column>
             <CourseReviewCard code="COMP1531" name="Software Engineering Fundamentals"
-              desc="This is a placeholder for the actual course description."/>
+              desc="This course provides an introduction to software engineering
+               principles: basic software lifecycle concepts, modern development
+                methodologies, conceptual modeling and how these activities
+                relate to programming. It also introduces the basic notions of
+                team-based project management via conducting a project to design
+                , build and deploy a simple web-based application. It is
+                typically taken in the term after completing COMP1511, but could
+                 be delayed and taken later. It provides essential background
+                 for the teamwork and project management required in many later
+                 courses."/>
           </Grid.Column>
         </Grid.Row>
 
         <Grid.Row>
           <Grid.Column>
             <CourseReviewCard code="COMP2511" name="Object Oriented Progamming"
-              desc="This is a placeholder for the actual course description."/>
+              desc="This course aims to introduce students to the principles of
+              object-oriented design and to fundamental techniques in
+              object-oriented programming. It is typically taken in the second
+              year of study, after COMP2521, to ensure an appropriate background
+               in data structures. The knowledge gained in COMP2511 is useful in
+                a wide range of later-year CS courses."/>
           </Grid.Column>
           <Grid.Column>
             <CourseReviewCard code="COMP2521" name="Data Structures and Algorithms"
-              desc="This is a placeholder for the actual course description."/>
+              desc="The goal of this course is to deepen students' understanding
+               of data structures and algorithms and how these can be employed
+               effectively in the design of software systems. We anticipate that
+                it will generally be taken in the second year of a program, but
+                since its only pre-requisite is COMP1511, is it possible to take
+                 it in first year. It is an important course in covering a range
+                  of core data structures and algorithms that will be used in
+                  context in later courses."/>
           </Grid.Column>
           <Grid.Column>
             <CourseReviewCard code="SENG2011" name="Workshop on Reasoning about Programs"
-              desc="This is a placeholder for the actual course description."/>
+              desc="This is a workshop course aimed at developing the skills of
+              writing precise specifications of programs and translating these
+              specifications into correct implementations. The course applies
+              the rigorous modelling and verification techniques introduced in
+              COMP2111 to a diverse and increasingly complex set of problems.
+              Further methods for reasoning about programs are introduced,
+              including methods for reasoning about termination,  program
+              refinement and data refinement. The primary learning outcome is to
+               develop students' abilities to apply these ideas to structure
+               their thinking about programs, but the course may use a formal
+               verification tool to support learning."/>
           </Grid.Column>
         </Grid.Row>
       </Grid>
