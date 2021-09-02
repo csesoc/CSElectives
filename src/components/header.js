e9279fd3efa4483a89d00d2c4c114fb69e25cd60
Priscilla <tingtangsoo@gmail.com>
2021-09-02 12:57:17 +1000

    Working on header style

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Image, Menu } from 'semantic-ui-react';
+import { Button, Container, Image, Menu, Input } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 import Logo from '../assets/logo.svg';
@@ -13,43 +13,49 @@ const Header = () => {
 
   return (
     <Menu pointing secondary>
-      <Menu.Item
-        as={Link}
-        to="/"
-        name='home'
-        onClick={handleItemClick}
-      >
-        <Image src={Logo}/>
-      </Menu.Item>
-      <Menu.Item
-        as={Link}
-        to="/course"
-        name='courses'
-        active={activeItem === 'courses'}
-        onClick={handleItemClick}
-      >
+      <Container>
+        <Menu.Item
+          as={Link}
+          to="/"
+          name='home'
+          onClick={handleItemClick}
+        >
+          <Image src={Logo} className='logo-size'/>
+        </Menu.Item>
+        <Menu.Item
+          as={Link}
+          to="/course"
+          name='courses'
+          active={activeItem === 'courses'}
+          onClick={handleItemClick}
+        >
       Courses
-      </Menu.Item>
-      <Menu.Item
-        as={Link}
-        to="/review"
-        name='review'
-        active={activeItem === 'review'}
-        onClick={handleItemClick}
-      >
+        </Menu.Item>
+        <Menu.Item
+          as={Link}
+          to="/review"
+          name='review'
+          active={activeItem === 'review'}
+          onClick={handleItemClick}
+        >
       Submit a review
-      </Menu.Item>
-      <Menu.Item
-        as={Link}
-        to="/login"
-        name='login'
-        onClick={handleItemClick}
-        position='right'
-      >
-        <Button>Log In</Button>
-      </Menu.Item>
+        </Menu.Item>
+        <Menu.Item
+          position='right'
+        >
+          <Input icon='search' placeholder='COMP1511' />
+        </Menu.Item>
+        <Menu.Item
+          as={Link}
+          to="/login"
+          name='login'
+          onClick={handleItemClick}
+          position='right'
+        >
+          <Button>Log In</Button>
+        </Menu.Item>
+      </Container>
     </Menu>
-
   );
 };
 
