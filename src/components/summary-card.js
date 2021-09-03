4c2aa03ab94bc4a49e69d0f12c64c97f23d791a8
vvwangg <vivianwang292@gmail.com>
2021-09-03 15:30:46 +1000

    removed extra space on sides and removed card from summary

--- src/components/summary-card.js
+++ src/components/summary-card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Label, Grid, Card, Progress, Rating } from 'semantic-ui-react';
+import { Header, Grid, Progress, Rating } from 'semantic-ui-react';
 import LabelExampleIcon from '../components/tags.js';
 
 
@@ -11,65 +11,61 @@ const SummaryCard = (props) => {
 
   return (
     <div style={{ display: 'block', margin: '20px' }}>
-      <Card fluid raised>
-        <Card.Content>
-          <Grid>
-            <Grid.Row className='my-grid-top'>
-              <div className='my-course-page-tags'>
-                <LabelExampleIcon code="Level 1"/>
-              </div>
-            </Grid.Row>
-            <Grid.Row className='my-grid-row'>
-              <div>
-                <Card.Header as='h2'>{summaryTitle}</Card.Header>
-                <a href={summaryLink} rel="noreferrer" target="_blank">
+      <Grid>
+        <Grid.Row className='my-grid-top'>
+          <div className='my-course-page-tags'>
+            <LabelExampleIcon code="Level 1"/>
+          </div>
+        </Grid.Row>
+        <Grid.Row className='my-grid-row'>
+          <div>
+            <Header as='h2'>{summaryTitle}</Header>
+            <a href={summaryLink} rel="noreferrer" target="_blank">
                     COMP1511 Handbook
-                </a> <br/>
-                <Rating icon='star' defaultRating={overallRating} maxRating={5} disabled/>
-                <Card.Meta>{numReviews} Reviews</Card.Meta>
-              </div>
-            </Grid.Row>
-            <Grid.Row className='my-grid-row'>
-              <div style={{ width: '100%' }}>
-                <span>
+            </a> <br/>
+            <Rating icon='star' defaultRating={overallRating} maxRating={5} disabled/>
+            {numReviews} Reviews
+          </div>
+        </Grid.Row>
+        <Grid.Row className='my-grid-row'>
+          <div style={{ width: '100%' }}>
+            <span>
                   Usefulness
-                </span>
-                <Progress className='bar-chart' color='blue' value={usefulAvg} total='5' progress='ratio'
-                  size='standard'/>
-                <span>
+            </span>
+            <Progress className='bar-chart' color='blue' value={usefulAvg} total='5' progress='ratio'
+              size='standard'/>
+            <span>
                   Workload
-                </span>
-                <Progress className='bar-chart' color='blue' value={workloadAvg} total='5' progress='ratio'
-                  size='standard'/>
-                <span>
+            </span>
+            <Progress className='bar-chart' color='blue' value={workloadAvg} total='5' progress='ratio'
+              size='standard'/>
+            <span>
                   Enjoyment
-                </span>
-                <Progress className='bar-chart' color='blue' value={enjoymentAvg} total='5' progress='ratio'
-                  size='standard'/>
-                <span>
+            </span>
+            <Progress className='bar-chart' color='blue' value={enjoymentAvg} total='5' progress='ratio'
+              size='standard'/>
+            <span>
                   Difficulty
-                </span>
-                <Progress className='bar-chart' color='blue' value={difficultyAvg} total='5' progress='ratio'
-                  size='standard'/>
-              </div>
-            </Grid.Row>
-            <Grid.Row className='my-grid-row'>
-              <Card.Content>
-                {summaryDesc}
-              </Card.Content>
-            </Grid.Row>
-            <Grid.Row className='my-grid-bottom'>
-              <Card.Content>
-                <h4>Topics</h4>
-                <li>topic one</li>
-                <li>topic two</li>
-                <li>topic three</li>
-                <li>topic four</li>
-              </Card.Content>
-            </Grid.Row>
-          </Grid>
-        </Card.Content>
-      </Card>
+            </span>
+            <Progress className='bar-chart' color='blue' value={difficultyAvg} total='5' progress='ratio'
+              size='standard'/>
+          </div>
+        </Grid.Row>
+        <Grid.Row className='my-grid-row'>
+          {summaryDesc}
+        </Grid.Row>
+        <Grid.Row className='my-grid-bottom'>
+          <div>
+            <h4>Topics</h4>
+            <li>topic one</li>
+            <li>topic two</li>
+            <li>topic three</li>
+            <li>topic four</li>
+          </div>
+        </Grid.Row>
+      </Grid>
+
+
     </div>
   );
 };
--- src/pages/course-page.js
+++ src/pages/course-page.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header, Label, Progress, Grid, Dropdown, Button } from 'semantic-ui-react';
+import { Grid, Dropdown, Button } from 'semantic-ui-react';
 import ReviewCard from '../components/review-card.js';
 import SummaryCard from '../components/summary-card.js';
 import { useHistory } from 'react-router-dom';
@@ -13,119 +13,115 @@ const CoursePage = () => {
   return (
     <>
       <div>
-        <Grid columns={2} stackable>
-          <Grid.Row>
-            <Grid.Column width={6}>
-              <div className="my-summary-card">
-                <SummaryCard
-                  summaryTitle="COMP1511 - Intro to Programming"
-                  summaryLink="https://www.handbook.unsw.edu.au/undergraduate/courses/2021/COMP1511/"
-                  overallRating="4"
-                  numReviews="42"
-                  summaryDesc=
-                    "This is a long description of the above course describing"
-                  usefulAvg="3"
-                  workloadAvg="2"
-                  difficultyAvg="4"
-                  enjoymentAvg="3"
-                />
-              </div>
-            </Grid.Column>
-            <Grid.Column >
-              <Grid.Column width={10}>
-                <Grid columns={3}>
-                  <Grid.Row>
-                    <Grid.Column width={4}>
-                      <div className='review-heading'>
-                        <h2>Reviews</h2>
-                      </div>
-                    </Grid.Column>
-                    <Grid.Column width={4}>
-                      <div className='sort-reviews'>
-                        <Dropdown text = 'Sort by'>
-                          <Dropdown.Menu>
-                            <Dropdown.Item text='Most Popular' />
-                            <Dropdown.Item text='Most Recent' />
-                          </Dropdown.Menu>
-                        </Dropdown>
-                      </div>
-                    </Grid.Column>
-                    <Grid.Column width={7}>
-                      <Button class="ui button" onClick={handleClick} className='review-button'>
+        <Grid stackable>
+          <Grid.Column width={7} floated='left'>
+            <div className="my-summary-card">
+              <SummaryCard
+                summaryTitle="COMP1511 - Intro to Programming"
+                summaryLink="https://www.handbook.unsw.edu.au/undergraduate/courses/2021/COMP1511/"
+                overallRating="4"
+                numReviews="42"
+                summaryDesc=
+                  "This is a long description of the above course describing"
+                usefulAvg="3"
+                workloadAvg="2"
+                difficultyAvg="4"
+                enjoymentAvg="3"
+              />
+            </div>
+          </Grid.Column>
+          <Grid.Column width={9} floated='right'>
+            <Grid columns={3}>
+              <Grid.Row>
+                <Grid.Column width={4}>
+                  <div className='review-heading'>
+                    <h2>Reviews</h2>
+                  </div>
+                </Grid.Column>
+                <Grid.Column width={4}>
+                  <div className='sort-reviews'>
+                    <Dropdown text = 'Sort by'>
+                      <Dropdown.Menu>
+                        <Dropdown.Item text='Most Popular' />
+                        <Dropdown.Item text='Most Recent' />
+                      </Dropdown.Menu>
+                    </Dropdown>
+                  </div>
+                </Grid.Column>
+                <Grid.Column width={7}>
+                  <Button class="ui button" onClick={handleClick} className='review-button'>
                         Submit a review
-                      </Button>
-                    </Grid.Column>
-                  </Grid.Row>
-                </Grid>
-                <div className="card-displayer">
-                  <ReviewCard
-                    overallRating="4"
-                    reviewDate="10/2/20"
-                    reviewTitle="wow i love this subject"
-                    usefulProgress="3"
-                    workloadProgress="2"
-                    enjoymentProgress="4"
-                    difficultyProgress="1"
-                  />
-                </div>
-                <div className="card-displayer">
-                  <ReviewCard
-                    overallRating="2"
-                    reviewDate="10/2/19"
-                    reviewTitle="its okay..."
-                    usefulProgress="2"
-                    workloadProgress="5"
-                    enjoymentProgress="3"
-                    difficultyProgress="2"
-                  />
-                </div>
-                <div className="card-displayer">
-                  <ReviewCard
-                    overallRating="2"
-                    reviewDate="10/2/19"
-                    reviewTitle="its okay..."
-                    usefulProgress="2"
-                    workloadProgress="5"
-                    enjoymentProgress="3"
-                    difficultyProgress="2"
-                  />
-                </div>
-                <div className="card-displayer">
-                  <ReviewCard
-                    overallRating="2"
-                    reviewDate="10/2/19"
-                    reviewTitle="its okay..."
-                    usefulProgress="2"
-                    workloadProgress="5"
-                    enjoymentProgress="3"
-                    difficultyProgress="2"
-                  />
-                </div>
-                <div className="card-displayer">
-                  <ReviewCard
-                    overallRating="2"
-                    reviewDate="10/2/19"
-                    reviewTitle="its okay..."
-                    usefulProgress="2"
-                    workloadProgress="5"
-                    enjoymentProgress="3"
-                    difficultyProgress="2"
-                  />
-                </div>
-                <div className="card-displayer">
-                  <ReviewCard
-                    overallRating="2"
-                    reviewDate="10/2/19"
-                    reviewTitle="its okay..."
-                    usefulProgress="2"
-                    workloadProgress="5"
-                    enjoymentProgress="3"
-                    difficultyProgress="2"
-                  />
-                </div>
-              </Grid.Column>
-            </Grid.Column>
-          </Grid.Row>
+                  </Button>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+            <div className="card-displayer">
+              <ReviewCard
+                overallRating="4"
+                reviewDate="10/2/20"
+                reviewTitle="wow i love this subject"
+                usefulProgress="3"
+                workloadProgress="2"
+                enjoymentProgress="4"
+                difficultyProgress="1"
+              />
+            </div>
+            <div className="card-displayer">
+              <ReviewCard
+                overallRating="2"
+                reviewDate="10/2/19"
+                reviewTitle="its okay..."
+                usefulProgress="2"
+                workloadProgress="5"
+                enjoymentProgress="3"
+                difficultyProgress="2"
+              />
+            </div>
+            <div className="card-displayer">
+              <ReviewCard
+                overallRating="2"
+                reviewDate="10/2/19"
+                reviewTitle="its okay..."
+                usefulProgress="2"
+                workloadProgress="5"
+                enjoymentProgress="3"
+                difficultyProgress="2"
+              />
+            </div>
+            <div className="card-displayer">
+              <ReviewCard
+                overallRating="2"
+                reviewDate="10/2/19"
+                reviewTitle="its okay..."
+                usefulProgress="2"
+                workloadProgress="5"
+                enjoymentProgress="3"
+                difficultyProgress="2"
+              />
+            </div>
+            <div className="card-displayer">
+              <ReviewCard
+                overallRating="2"
+                reviewDate="10/2/19"
+                reviewTitle="its okay..."
+                usefulProgress="2"
+                workloadProgress="5"
+                enjoymentProgress="3"
+                difficultyProgress="2"
+              />
+            </div>
+            <div className="card-displayer">
+              <ReviewCard
+                overallRating="2"
+                reviewDate="10/2/19"
+                reviewTitle="its okay..."
+                usefulProgress="2"
+                workloadProgress="5"
+                enjoymentProgress="3"
+                difficultyProgress="2"
+              />
+            </div>
+          </Grid.Column>
         </Grid>
       </div>
     </>
