ac2c2b88239117566c07ede6ae00662d4258c47c
Joey <joeyxie12@gmail.com>
2021-09-04 18:00:56 +1000

    not found page complete :))git add .

--- src/pages/not-found-page.js
+++ src/pages/not-found-page.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import { Icon, Button, Header } from 'semantic-ui-react';
-import Error404Image from '../assets/not-found.jpg';
+import { Icon, Button } from 'semantic-ui-react';
 import { useHistory } from 'react-router-dom';
 
 const NotFoundPage = () => {
   const history = useHistory();
   const handleClick = () => {
     console.log('clicked or not??');
-    history.push('/home');
+    history.push('/');
   };
   return (
-    <div className='not-found-page-background'>
-      {/* <Header as='h1'>404 Not Found Page</Header> */}
-      <h1 className='not-found-page-text'>
+    <>
+      <div className='not-found-page-background'/>
+      <div className='not-found-page-text'>
         <div>Oops the page you are looking for cannot be found {'>.<'}</div>
         <div>Looks like you might be lost in the CyberSpacE...</div>
-        <Button icon onClick={handleClick} labelPosition='left' size='big' inverted >
-          <Icon name='home'/>
-        Click to return Home
+        <Button animated onClick={handleClick} size='big' inverted >
+          <Button.Content visible> Teleport me back home </Button.Content>
+          <Button.Content hidden>
+            <Icon name='space shuttle' />
+          </Button.Content>
         </Button>
-      </h1>
-
-    </div>
+      </div>
+    </>
   );
 };
 
