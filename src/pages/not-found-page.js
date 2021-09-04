1f2b6627edc852666d05c7fe0547017335dc0755
Joey <joeyxie12@gmail.com>
2021-09-04 17:32:35 +1000

    fixed styling and added non-clickable button

--- src/pages/not-found-page.js
+++ src/pages/not-found-page.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { Header, Image } from 'semantic-ui-react';
+import { Icon, Button, Header } from 'semantic-ui-react';
 import Error404Image from '../assets/not-found.jpg';
+import { useHistory } from 'react-router-dom';
 
 const NotFoundPage = () => {
+  const history = useHistory();
+  const handleClick = () => {
+    console.log('clicked or not??');
+    history.push('/home');
+  };
   return (
     <div className='not-found-page-background'>
       {/* <Header as='h1'>404 Not Found Page</Header> */}
-      <h1 align="center">
-      Oops looks like you might be lost in the CyberSpacE
-        <br></br>
+      <h1 className='not-found-page-text'>
+        <div>Oops the page you are looking for cannot be found {'>.<'}</div>
+        <div>Looks like you might be lost in the CyberSpacE...</div>
+        <Button icon onClick={handleClick} labelPosition='left' size='big' inverted >
+          <Icon name='home'/>
+        Click to return Home
+        </Button>
       </h1>
+
     </div>
   );
 };
