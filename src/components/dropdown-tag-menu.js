75cbe585ba3ab692eb2694e748519e122b51947e
Timmy Huang <timmyme2@gmail.com>
2021-09-05 16:34:58 +1000

    Revert "Started creating checkbox component"
    This reverts commit 06f6ec84434050ca71f4eaa18b0a7ced31f64d85.

--- src/components/dropdown-tag-menu.js
+++ src/components/dropdown-tag-menu.js
@@ -1,40 +1,26 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-import { Dropdown, Checkbox } from 'semantic-ui-react';
+import { Dropdown } from 'semantic-ui-react';
 
 const DropdownTags = (props) => {
   const { title, tagOptions, activeTags, setActiveTags } = props;
 
-  const toggleSelection = (e, { label, checked }) => {
-    if (checked) {
-      setActiveTags([...activeTags, label]);
-    } else {
-      setActiveTags(activeTags.filter((el) => el !== label));
-    }
-    console.log(activeTags);
+  const tagItems = (x) => {
+    return (<Dropdown.Item>{x}</Dropdown.Item>);
   };
 
-  const tagItems = (object) => {
-    return (
-      <Dropdown.Item key={object.key}>
-        <Checkbox label={object.value} onChange={toggleSelection} />
-      </Dropdown.Item>
-    );
-  };
   // Need to change how an item is on the tag menu is displayed after selected
 
   return (
     <Dropdown
-      text = {title}
-      item
-      simple
+      placeholder = {title}
+      selection
+      clearable
       className='icon'
+
+      options = {tagOptions}
     >
-      <Dropdown.Menu>
-        <Dropdown.Header icon='tags' content='Filter by tag' />
-        {tagOptions.map(tagItems)}
-      </Dropdown.Menu>
     </Dropdown>
   );
 };
@@ -47,33 +33,3 @@ DropdownTags.propTypes = {
 };
 
 export default DropdownTags;
-
-/*
-export default function App() {
-  const [selection, setSelection] = useState([]);
-
-  const toggleSelection = (e, { label, checked }) => {
-    if (checked) {
-      setSelection([...selection, label]);
-    } else {
-      setSelection(selection.filter(el => el !== label));
-    }
-  };
-
-  return (
-    <Segment basic>
-      <p>Selection: {selection.length > 0 ? selection.join(", ") : "empty"}</p>
-
-      <Dropdown item simple text="Select sports">
-        <Dropdown.Menu>
-          {sports.map(({ id, title }) => (
-            <Dropdown.Item key={id}>
-              <Checkbox label={title} onChange={toggleSelection} />
-            </Dropdown.Item>
-          ))}
-        </Dropdown.Menu>
-      </Dropdown>
-    </Segment>
-  );
-}
-*/
