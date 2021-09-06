6c55e9103a50db07aa610037e5f28a7d61c7a987
Joey <joeyxie12@gmail.com>
2021-09-06 13:17:42 +1000

    added anonymity selection

--- src/pages/review-page.js
+++ src/pages/review-page.js
@@ -1,89 +1,90 @@
 import React, { useState } from 'react';
 import { Checkbox, Form, Header, Input, TextArea, Radio, Dropdown } from 'semantic-ui-react';
 import '../styles/review-page.css';
-
-import TermTakenSelection from '../components/term-taken-dropdown.js';
+const termOptions = [
+  { value: '21T2', text: '21T2' },
+  { value: '21T1', text: '21T1' },
+  { value: '20T3', text: '20T3' },
+  { value: '20T2', text: '20T2' },
+  { value: '20T1', text: '20T1' },
+  { value: '19T3', text: '19T3' },
+  { value: '19T2', text: '19T2' },
+  { value: '19T1', text: '19T1' },
+  { value: '18S2', text: '18S2' },
+  { value: '18S1', text: '18S1' },
+  { value: '17S2', text: '17S2' },
+  { value: '17S1', text: '17S1' },
+  { value: '16S2', text: '16S2' },
+  { value: '16S1', text: '16S1' },
+];
 
 const ReviewPage = () => {
   const [question, setQuestion] = useState('');
   const handleQuestionChange = (e, { value }) => {
     setQuestion(value);
   };
-  const termOptions = [
-    { value: '21T2', text: '21T2' },
-    { value: '21T1', text: '21T1' },
-    { value: '20T3', text: '20T3' },
-    { value: '20T2', text: '20T2' },
-    { value: '20T1', text: '20T1' },
-    { value: '19T3', text: '19T3' },
-    { value: '19T2', text: '19T2' },
-    { value: '19T1', text: '19T1' },
-    { value: '18S2', text: '18S2' },
-    { value: '18S1', text: '18S1' },
-    { value: '17S2', text: '17S2' },
-    { value: '17S1', text: '17S1' },
-    { value: '16S2', text: '16S2' },
-    { value: '16S1', text: '16S1' },
-  ];
+
+  const [anonymity, setAnonymity] = useState('');
+  const handleAnonymityChange = (e, { value }) => {
+    setAnonymity(value);
+  };
+
 
   return (
     <>
       <Header as='h1'>Submit Review Page</Header>
 
-      {/* Input component: https://react.semantic-ui.com/elements/input/ */}
-      <Input placeholder="You'll need a text box!" />
-      <br />
-
-      {/* Checkbox component: https://react.semantic-ui.com/modules/checkbox/ */}
-      <Checkbox label='You might want checkboxes...' />
-      <br />
-
-      {/* Radio button component: https://react.semantic-ui.com/modules/checkbox/#types-radio */}
-      <Checkbox radio label='Or some radio buttons which in code is actually a checkbox lol...' />
-      <br />
-
-      {/* TextArea compoennt: https://react.semantic-ui.com/addons/text-area/ */}
       <Form>
-        <TextArea placeholder='Maybe use this for reviews??????????' />
-      </Form>
-
-      <Form>
+        <Form.Group inline>
+          <Form.Field>
+            Are you:
+            <Form.Radio
+              label='currently undertaking the course'
+              name='completionGroup'
+              value='undertaking'
+              checked={question === 'undertaking'}
+              onChange={handleQuestionChange}
+            />
+          </Form.Field>
+          <Form.Field>
+            or have you:
+            <Form.Radio
+              label='completed the course'
+              name='completionGroup'
+              value='completed'
+              checked={question === 'completed'}
+              onChange={handleQuestionChange}
+            />
+          </Form.Field>
+          <Form.Field>
+            When did you complete it?
+            <Form.Dropdown
+              placeholder='Select term taken'
+              fluid
+              search
+              selection
+              options={termOptions}
+            />
+          </Form.Field>
+        </Form.Group>
         <Form.Field>
-          Are you:
-        </Form.Field>
-        <Form.Field>
-          <Radio
-            label='currently undertaking the course'
-            name='radioGroup'
-            value='undertaking'
-            checked={question === 'undertaking'}
-            onChange={handleQuestionChange}
+          Would you like to remain anonymous? <b>{anonymity}</b>
+          <Form.Radio
+            label='Yes'
+            name='anonymityGroup'
+            value='Yes'
+            checked={anonymity === 'Yes'}
+            onChange={handleAnonymityChange}
+          />
+          <Form.Radio
+            label='No'
+            name='anonymityGroup'
+            value='No'
+            checked={anonymity === 'No'}
+            onChange={handleAnonymityChange}
           />
         </Form.Field>
-        <Form.Field>
-          or have you:
-        </Form.Field>
-        <Form.Field>
-          <Radio
-            label='completed the course'
-            name='radioGroup'
-            value='completed'
-            checked={question === 'completed'}
-            onChange={handleQuestionChange}
-          />
-        </Form.Field>
-        <Form.Field>
-          When did you complete it?
-        </Form.Field>
       </Form>
-
-      <Dropdown
-        placeholder='Select term taken'
-        fluid
-        search
-        selection
-        options={termOptions}
-      />
     </>
   );
 };
