50e964180f927c1e88742648b3885e18440061f8
Cameron Huang <cameronhuang96@gmail.com>
2021-09-06 14:04:11 +1000

    removed commented function from home-page.js

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -68,39 +68,6 @@ const HomePage = (props) => {
   const [activeTags, setActiveTags] = useState([]);
   const [query, setQuery] = useState('Home Page');
 
-  // Returns an array of courses sorted in descending order of number of reviews
-  const sortMostReviewed = () => {
-    return Object.values(courses).sort(function(a, b) {
-      return b.reviews.length - a.reviews.length;
-    });
-  };
-
-  // // This function creates the grid of course review cards
-  // const buildGrid = () => {
-  //   const sortedCourses = sortMostReviewed();
-  //   const gridArray = [];
-  //   const colSize = 3;
-  //   for (let i = 0; i < sortedCourses.length; i += colSize) {
-  //     const gridRow = sortedCourses.slice(i, i + colSize);
-  //     gridArray.push(gridRow);
-  //   }
-  //   return gridArray.map((row, index) => {
-  //     return (
-  //       <Grid.Row key={index}>
-  //         {row.map((course) => (
-  //           <Grid.Column key={course.id}>
-  //             <CourseReviewCard
-  //               code={course.courseCode}
-  //               name={course.title}
-  //               desc={course.description}
-  //               numReviews={course.reviews.length}
-  //             />
-  //           </Grid.Column>))}
-  //       </Grid.Row>
-  //     );
-  //   });
-  // };
-
   const handleQueryChange = (e, { value }) => {
     setQuery(value);
     console.log(query);
