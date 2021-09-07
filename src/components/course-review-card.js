832cd94efb12443373a636abbe601ac9166f4c5e
Cameron Huang <cameronhuang96@gmail.com>
2021-09-07 16:41:04 +1000

    cards link to their corresponding course page when clicked

--- src/components/course-review-card.js
+++ src/components/course-review-card.js
@@ -3,24 +3,28 @@ import PropTypes from 'prop-types';
 
 import { Card } from 'semantic-ui-react';
 import { Rating } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 const CourseReviewCard = (props) => {
   const { code, name, desc, numReviews } = props;
   const displayDesc = desc.length > 220 ? desc.substring(220, length) + '...' : desc;
+  const page = 'course/' + code;
 
   return (
     <div className='card-container'>
-      <Card className='course-review-card'>
-        <Card.Content>
-          <Card.Header>{code}</Card.Header>
-          <Card.Meta>{name}</Card.Meta>
-          <Rating icon='star' defaultRating={3} maxRating={5} disabled />
-          <Card.Meta> {numReviews} reviews </Card.Meta>
-          <Card.Description>
-            {displayDesc}
-          </Card.Description>
-        </Card.Content>
-      </Card>
+      <Link to={page}>
+        <Card className='course-review-card'>
+          <Card.Content>
+            <Card.Header>{code}</Card.Header>
+            <Card.Meta>{name}</Card.Meta>
+            <Rating icon='star' defaultRating={3} maxRating={5} disabled />
+            <Card.Meta> {numReviews} reviews </Card.Meta>
+            <Card.Description>
+              {displayDesc}
+            </Card.Description>
+          </Card.Content>
+        </Card>
+      </Link>
     </div>
   );
 };
