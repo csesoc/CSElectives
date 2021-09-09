9636bfb0dec1ac5a5c22c730e1a542e54db68192
Hayeselnut <hayeselnut2127@gmail.com>
2021-09-09 15:31:07 +1000

    refactored database class to reflect new structure in firestore

--- src/db/db.js
+++ src/db/db.js
@@ -3,6 +3,9 @@ import { getDocs, getFirestore, collection, addDoc } from 'firebase/firestore/li
 
 import FirebaseConfig from '../assets/firebase-config.json';
 
+// NOTE: Courses and reviews have been separated to support an easier transition
+// for lazy loading in the future
+
 /**
  * Class responsible for reading and writing to Firestore database.
  */
@@ -25,6 +28,52 @@ class Database {
     return await getDocs(col);
   }
 
+  /**
+   * Gets all courses and reviews, with reviews nested in the course object
+   * @return {object}
+   */
+  async getCoursesAndReviews() {
+    const courses = await this.getCourses();
+    const reviews = await this.getReviews();
+
+    Object.keys(courses).forEach((courseCode) => {
+      courses[courseCode].reviews = courses[courseCode].reviews.map((reviewId) => {
+        return reviews[reviewId];
+      });
+    });
+
+    return courses;
+  }
+
+  /**
+   * Gets all courses from database
+   * @return {object}
+   */
+  async getCourses() {
+    const courses = {};
+
+    const coursesSnapshot = await this.getSnapshot('courses');
+    coursesSnapshot.docs.forEach((doc) => {
+      courses[doc.id] = doc.data();
+    });
+
+    return courses;
+  }
+
+  /**
+   * Gets all reviews from database
+   * @return {object}
+   */
+  async getReviews() {
+    const reviews = {};
+
+    const reviewsSnapshot = await this.getSnapshot('reviews');
+    reviewsSnapshot.docs.forEach((doc) => {
+      reviews[doc.id] = doc.data();
+    });
+
+    return reviews;
+  }
 
   /**
    * Appends a new review to the course's reviews
