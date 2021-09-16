0c34c6eed2beafb6b67dcc00a62909464948717e
Timmy Huang <timmyme2@gmail.com>
2021-09-17 02:14:07 +1000

    Created a start on the filter functions

--- src/components/card-grid.js
+++ src/components/card-grid.js
@@ -6,13 +6,34 @@ import { Grid } from 'semantic-ui-react';
 
 // This function creates the grid of course review cards
 const CardGrid = (props) => {
-  const { courses } = props;
+  const { courses, activeMajorTags, activeTermTags, activePrefixTags } = props;
   // Returns an array of courses sorted in descending order of number of reviews
   const sortMostReviewed = () => {
     return Object.values(courses).sort(function(a, b) {
       return b.reviews.length - a.reviews.length;
     });
   };
+
+  const filterMajors = () => {
+    courses.filter((courses) => (
+      activeMajorTags.includes()
+    ));
+  };
+
+  const filterTerms = (courses) => {
+    courses.filter((course) => (
+      activeTermTags.includes(course.term)
+    ));
+    return courses;
+  };
+
+  const filterPrefix = (courses) => {
+    courses.filter((course) => (
+      activePrefixTags.includes(course.slice(0, 4))
+    ));
+    return courses;
+  };
+
 
   const sortedCourses = sortMostReviewed();
   const gridArray = [];
@@ -40,6 +61,9 @@ const CardGrid = (props) => {
 
 CardGrid.propTypes = {
   courses: PropTypes.object,
+  activeMajorTags: PropTypes.array,
+  activeTermTags: PropTypes.array,
+  activePrefixTags: PropTypes.array,
 };
 
 export default CardGrid;
