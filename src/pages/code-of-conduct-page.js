4875232c8e8c0851d7de2e7419e9510a13ddecf9
Victoria Vu <victoriavu@Victorias-MacBook-Pro.local>
2022-02-01 16:11:10 +1100

    finalised tncs page

--- src/pages/code-of-conduct-page.js
+++ src/pages/code-of-conduct-page.js
@@ -5,7 +5,9 @@ const CodeOfConductPage = () => {
     <div>
       <h1>Terms and Conditions</h1>
 
-      <h3>1. Your review must abide by the UNSW Student Code of Conduct</h3>
+      <h3>1. Your review must abide by the
+        <a href="https://www.gs.unsw.edu.au/policy/documents/studentcodepolicy.pdf"> UNSW Student Code of Conduct</a>
+      </h3>
 
       In addition to abiding by the Student Code of Conduct, we want to
       highlight the relevant sections, such as sections 4 and 5.3, which are outlined as follows:
@@ -57,47 +59,87 @@ const CodeOfConductPage = () => {
 
       <h3>2. Reviews must be relevant and based on real experiences</h3>
       <ul>
-        2.1. Spam and fake content: Reviews ought to reflect genuine experiences of being a
-        student enrolled in the course being reviewed. Additionally reviews shouldn&apos;t be posted
-        for the purpose of manipulating a course&apos;s ratings. Don&apos;t post fake reviews and don&apos;t post
-        a review multiple times.
+        <li>
+          2.1. Spam and fake content: Reviews ought to reflect genuine experiences of being a
+          student enrolled in the course being reviewed. Additionally reviews shouldn&apos;t be posted
+          for the purpose of manipulating a course&apos;s ratings. Don&apos;t post fake reviews and don&apos;t post
+          a review multiple times.
+        </li>
+        <li>2.2. Off-topic: Only post reviews based on your experience while being enrolled in the relevant
+          UNSW course. Our platform is not meant to be a forum for solicitation of goods and services,
+          political or social commentary, or any other unrelated topics. Reviews which fail to meet this will
+          be removed.
+        </li>
+        <li>2.3. Illegal content: Any review that describes or links to illegal activity will be removed.
+          This includes but is not limited to; sexual abuse imagery, descriptions of illegal products or
+          services, descriptions of graphic violence, or anything that violates any laws, rules or regulations.
+        </li>
+        <li>2.4. Offensive content: Any review that contain profane, obscene or offensive language, will be
+          removed.
+        </li>
+        <li>2.5. Malicious content: Any review that attempts to distribute or upload material that contain
+          malicious code or any other potentially harmful program or material, will be removed.
+        </li>
+        <li>
+          2.6. Dangerous Content: Our platform is a place for safe communication, for this reason we don&apos;t
+          permit reviewers to post dangerous content, including descriptions that:
+          <ul>
+            <li>
+              2.6.1. Threatens or advocates harm on oneself or others.
+            </li>
+            <li>2.6.2. Harasses, intimidates or bullies an individual or group of individuals.</li>
+            <li>2.6.3. Promotes discrimination of an individual or group based on their race, religion, disability,
+              age, nationality, sexual orientation, gender identity or any other characteristic that is associated
+              with discrimination.
+            </li>
+          </ul>
+        </li>
+        <li>
+          2.7. Impersonation: We don&apos;t allow individuals to write deceptive reviews as well as undertake
+          false representations.
+        </li>
+        <li>
+          2.8. Conflict of interest: Reviews are most valuable when they are unbiased and honest. An example
+          of a practice we would disapprove of; if a staff member for the course, writes a review for a term
+          for which they were not a student.
+        </li>
+      </ul>
 
-
-        2.6.1. Threatens or advocates harm on oneself or others.
-        2.6.2. Harasses, intimidates or bullies an individual or group of individuals.
-        2.6.3. Promotes discrimination of an individual or group based on their race, religion, disability,
-        age, nationality, sexual orientation, gender identity or any other characteristic that is associated
-        with discrimination.
-        2.7. Impersonation: We don&apos;t allow individuals to write deceptive reviews as well as undertake
-        false representations.
-        2.8. Conflict of interest: Reviews are most valuable when they are unbiased and honest. An example
-        of a practice we would disapprove of; if a staff member for the course, writes a review for a term
-        for which they were not a student.
-      </ul>
-      <ul>2.2. Off-topic: Only post reviews based on your experience while being enrolled in the relevant
-        UNSW course. Our platform is not meant to be a forum for solicitation of goods and services,
-        political or social commentary, or any other unrelated topics. Reviews which fail to meet this will be removed.
-      </ul>
-      <ul>2.3. Illegal content: Any review that describes or links to illegal activity will be removed.
-        This includes but is not limited to; sexual abuse imagery, descriptions of illegal products or
-        services, descriptions of graphic violence, or anything that violates any laws, rules or regulations.
-      </ul>
-      <ul>2.4. Offensive content: Any review that contain profane, obscene or offensive language, will be
-        removed.
-      </ul>
-      <ul>2.5. Malicious content: Any review that attempts to distribute or upload material that contain
-        malicious code or any other potentially harmful program or material, will be removed.
-      </ul>
+      In summary, reviews which are deliberately faked, off-topic, use defamatory language, or engage
+      in personal attacks are all in violation of our policy. We will remove reviews that violate our
+      terms & conditions and can suspend abusive contributors.
+      <h3>3. Reviews must be respectful</h3>
+      Non-exhaustive criterion:
       <ul>
-        2.6. Dangerous Content: Our platform is a place for safe communication, for this reason we don&apos;t
-        permit reviewers to post dangerous content, including descriptions that:
+        <li>2.1. Be thoughtful, fair and constructive.</li>
+        <li>2.2. Have a focus on the learning and teaching activities, provide relevant examples to
+          illustrate your point.
+        </li>
+        <li>2.3. Avoid comments which are personal or inappropriate such as comments about dress or appearance.</li>
+        <li>2.4. Avoid giving out private information, such as phone numbers, email addresses or social
+          media links.
+        </li>
+        <li>2.5. Do not write anything that is offensive, racist or sexist.</li>
       </ul>
+      Some examples of good reviews:
       <ul>
-
+        <li>Instead of saying &quot;The course was great.&quot;  explain why the class was great.
+          For example - &quot;The course was great. The lectures were well organised and presented and the
+          explanations were easy to follow. People were very talkative in tutorials and I learnt a lot from
+          my tutor who was very experienced. Additionally the teacher used visual examples that made things
+          easy to understand. Overall the content was difficult but very practical and interesting.&quot;.
+        </li>
+        <li>Instead of &quot;I couldn&apos;t understand anything the lecturer said, a boring and useless course&quot;,
+          try &quot;They should explain difficult concepts in depth, rather than relying on slides. When they
+          used jargon that we didn&apos;t know, I had trouble taking notes because I didn&apos;t understand the words
+          well enough. The content could be more engaging and useful if they showed practical usage of concepts
+          taught. &quot;
+        </li>
       </ul>
-      <ul>
+      <h3>4. Terms and conditions are subject to change within reason by the UNSW Computer Science and
+        Engineering Society Executive team.
+      </h3> <br />
 
-      </ul>
     </div>
   );
 };
