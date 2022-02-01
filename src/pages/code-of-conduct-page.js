06e21980d338a2e43b7b1c54afebdd36f0fd79e3
Victoria Vu <victoriavu@Victorias-MacBook-Pro.local>
2022-02-01 15:49:31 +1100

    terms and conditions formatting

--- src/pages/code-of-conduct-page.js
+++ src/pages/code-of-conduct-page.js
@@ -4,13 +4,100 @@ const CodeOfConductPage = () => {
   return (
     <div>
       <h1>Terms and Conditions</h1>
-      <br />
+
+      <h3>1. Your review must abide by the UNSW Student Code of Conduct</h3>
+
       In addition to abiding by the Student Code of Conduct, we want to
       highlight the relevant sections, such as sections 4 and 5.3, which are outlined as follows:
-      Section 4. Principles of open and respectful discussion
-      The following principles of open and respectful discussion are key to fostering
-      and preserving a safe and fair learning environment and a positive student experience at UNSW:
 
+      <br /> <br /><u>Section 4. Principles of open and respectful discussion</u> <br /> <br />
+
+      The following principles of open and respectful discussion are key to fostering
+      and preserving a safe and fair learning environment and a positive student experience at UNSW: <br />
+
+      <ul>
+        <li>UNSW encourages critical thought and open discussion, including of controversial ideas</li>
+        <li>UNSW is a dynamic community where ideas can change, and there is a commitment to creating
+          a safe space for exploring and debating changing perspectives
+        </li>
+        <li>Students should demonstrate respectful disagreement or feedback, expecting that they may
+          be asked to explain or give evidence for their perspective or claims
+        </li>
+        <li>Students are committed to engaging in respectful and inclusive conversations and debates;
+          turntaking; and appreciation
+        </li>
+        <li>Students accept differences amongst each other – both seen and unseen</li>
+        <li>Students must permit others to express disagreement without fear of reprisal.</li>
+
+      </ul>
+      <br />
+      <u>Section 5. Student Responsibilities: Responsibility 3: Observe standards of equity
+        and respect in dealing with every member of the UNSW community
+      </u>
+      <br /><br />
+      UNSW students:
+      <ul>
+        <li>treat all members of the UNSW community with courtesy and respect.
+          This includes respecting the rights of others to be treated equitably, free from all
+          forms of unlawful discrimination, harassment, bullying and vilification; and the rights
+          of others to express political and religious views
+        </li>
+        <li>respect University teaching, learning, academic or other activities by not behaving
+          in a way that significantly disrupts or interferes with these activities.
+          This includes any political, cultural, social or sporting gathering conducted by the
+          University or authorised by the University to be held on any of its campuses.
+
+        </li>
+        <li>comply with any reasonable direction or request from a UNSW staff member or
+          affiliate where the direction or request supports safety, good order and compliance
+          with UNSW policies and procedures.
+        </li>
+
+      </ul>
+
+      <h3>2. Reviews must be relevant and based on real experiences</h3>
+      <ul>
+        2.1. Spam and fake content: Reviews ought to reflect genuine experiences of being a
+        student enrolled in the course being reviewed. Additionally reviews shouldn&apos;t be posted
+        for the purpose of manipulating a course&apos;s ratings. Don&apos;t post fake reviews and don&apos;t post
+        a review multiple times.
+
+
+        2.6.1. Threatens or advocates harm on oneself or others.
+        2.6.2. Harasses, intimidates or bullies an individual or group of individuals.
+        2.6.3. Promotes discrimination of an individual or group based on their race, religion, disability,
+        age, nationality, sexual orientation, gender identity or any other characteristic that is associated
+        with discrimination.
+        2.7. Impersonation: We don&apos;t allow individuals to write deceptive reviews as well as undertake
+        false representations.
+        2.8. Conflict of interest: Reviews are most valuable when they are unbiased and honest. An example
+        of a practice we would disapprove of; if a staff member for the course, writes a review for a term
+        for which they were not a student.
+      </ul>
+      <ul>2.2. Off-topic: Only post reviews based on your experience while being enrolled in the relevant
+        UNSW course. Our platform is not meant to be a forum for solicitation of goods and services,
+        political or social commentary, or any other unrelated topics. Reviews which fail to meet this will be removed.
+      </ul>
+      <ul>2.3. Illegal content: Any review that describes or links to illegal activity will be removed.
+        This includes but is not limited to; sexual abuse imagery, descriptions of illegal products or
+        services, descriptions of graphic violence, or anything that violates any laws, rules or regulations.
+      </ul>
+      <ul>2.4. Offensive content: Any review that contain profane, obscene or offensive language, will be
+        removed.
+      </ul>
+      <ul>2.5. Malicious content: Any review that attempts to distribute or upload material that contain
+        malicious code or any other potentially harmful program or material, will be removed.
+      </ul>
+      <ul>
+        2.6. Dangerous Content: Our platform is a place for safe communication, for this reason we don&apos;t
+        permit reviewers to post dangerous content, including descriptions that:
+      </ul>
+      <ul>
+
+      </ul>
+      <ul>
+
+      </ul>
     </div>
   );
 };
