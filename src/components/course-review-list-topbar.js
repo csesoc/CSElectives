348fc1a429e1290f52b3e27979a3ca21d4775af4
Sean Wibisono <seanwibi@Seans-MBP.modem>
2022-03-09 23:06:43 +1100

    Alignment for listview topbar

--- src/components/course-review-list-topbar.js
+++ src/components/course-review-list-topbar.js
@@ -7,31 +7,31 @@ const TopBar = () => {
       <Item className='topbar'>
         <Item.Content className='topbar-contents-container'>
           <div className='topbar-item topbar-contents-code'>
-            <Item.Header className='item-header'>Course Code</Item.Header>
+            <Item.Header className='topbar-header'>Course Code</Item.Header>
           </div>
           <div className='topbar-item topbar-contents-name'>
-            <Item.Header className='item-header'>Course Name</Item.Header>
+            <Item.Header className='topbar-header'>Course Name</Item.Header>
           </div>
           <div className='topbar-item topbar-contents-tag'>
-            <Item.Header className='item-header'>Tag</Item.Header>
+            <Item.Header className='topbar-header'>Tags</Item.Header>
           </div>
           <div className='topbar-item topbar-contents-rating'>
-            <Item.Header className='item-header'>Overall Rating</Item.Header>
+            <Item.Header className='topbar-header'>Overall Rating</Item.Header>
           </div>
           <div className='topbar-item topbar-contents-enjoyability'>
-            <Item.Header className='item-header'>Enjoyability</Item.Header>
+            <Item.Header className='topbar-header'>Enjoyability</Item.Header>
           </div>
           <div className='topbar-item topbar-contents-usefulness'>
-            <Item.Header className='item-header'>Usefulness</Item.Header>
+            <Item.Header className='topbar-header'>Usefulness</Item.Header>
           </div>
           <div className='topbar-item topbar-contents-manageability'>
-            <Item.Header className='item-header'>Manageability</Item.Header>
+            <Item.Header className='topbar-header'>Manageability</Item.Header>
           </div>
           <div className='topbar-item topbar-contents-major'>
-            <Item.Header className='item-header'>Major</Item.Header>
+            <Item.Header className='topbar-header'>Major</Item.Header>
           </div>
           <div className='topbar-item topbar-contents-num'>
-            <Item.Header className='item-header'>No. of Reviews</Item.Header>
+            <Item.Header className='topbar-header'>No. of Reviews</Item.Header>
           </div>
         </Item.Content>
       </Item>
